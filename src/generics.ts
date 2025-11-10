4546db8268e78622ba21364e99003f48078dd59c
islamemon59 <islamemon59@gmail.com>
2025-11-10 23:06:13 +0600

    generics with array arrayObject with ts

---
+++ src/generics.ts
@@ -0,0 +1,23 @@
+//? Generic -> dynamically normalize
+
+type IsGeneric<value> = Array<value>;
+
+const friends: IsGeneric<string> = ["Mr.X", "Mr.Y", "Mr.Z"];
+const rollNumber: IsGeneric<number> = [1, 3, 5];
+const isBoolean: IsGeneric<boolean> = [true, false, true];
+
+//Tuple generic
+type Age<X, Y> = [X, Y];
+const age1: Age<number, number> = [20, 20];
+const age2: Age<string, string> = ["20", "20"];
+
+const user: IsGeneric<{name: string, age: number}> = [
+  {
+    name: "emon",
+    age: 20,
+  },
+  {
+    name: "Hossain",
+    age: 22,
+  },
+];
